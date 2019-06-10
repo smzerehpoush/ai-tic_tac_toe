ae1497d762a894e4c85272184ce8b6fd71bf561d
Seyyed Mahdiyar Zerehpoush <mahdiyar.zerehpoush@gmail.com>
2019-06-11 01:21:49 +0430

    fix bug of getWinner

--- src/Game.java
+++ src/Game.java
@@ -54,7 +54,7 @@ public class Game {
         try {
             if (board[0][0] != 0 && board[0][0] == board[1][1] && board[2][2] == board[1][1])
                 return board[1][1];
-            if (board[2][0] != 0 && board[2][0] == board[1][1] && board[2][2] == board[0][2])
+            if (board[2][0] != 0 && board[2][0] == board[1][1] && board[1][1] == board[0][2])
                 return board[1][1];
             for (int i = 0; i < boardSize; i++) {
                 if (board[i][0] != 0 && board[i][0] == board[i][1] && board[i][1] == board[i][2])
