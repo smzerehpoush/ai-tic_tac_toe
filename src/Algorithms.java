84873d4972cb9b39c2c35fe4b87dc0af5f51dda7
Seyyed Mahdiyar Zerehpoush <mahdiyar.zerehpoush@gmail.com>
2019-06-11 17:56:35 +0430

    add new impl to project

---
+++ src/Algorithms.java
@@ -0,0 +1,32 @@
+import java.util.Random;
+
+/**
+ * Uses various algorithms to play Tic Tac Toe.
+ */
+public class Algorithms {
+
+    /**
+     * Algorithms cannot be instantiated.
+     */
+    private Algorithms() {}
+
+
+    /**
+     * Play using the MiniMax Algorithm.
+     * @param board     the Tic Tac Toe board to play on
+     */
+    public static void miniMax (Board board) {
+        MiniMax.run(board.getTurn(), board, Double.POSITIVE_INFINITY);
+    }
+
+    /**
+     * Play using the MiniMax algorithm. Include a depth limit.
+     * @param board     the Tic Tac Toe board to play on
+     * @param ply       the maximum depth
+     */
+    public static void miniMax (Board board, int ply) {
+        MiniMax.run(board.getTurn(), board, ply);
+    }
+
+
+}
---
+++ src/Tic.java
@@ -0,0 +1,25 @@
+/**
+ * Created by mhd.zerehpoosh on 6/11/2019.
+ */
+public class Tic {
+    /**
+     * Play using the MiniMax Algorithm.
+     * @param board     the Tic Tac Toe board to play on
+     */
+    public static void miniMax (Board board) {
+        MiniMax.run(board.getTurn(), board, Double.POSITIVE_INFINITY);
+    }
+
+    /**
+     * Play using the MiniMax algorithm. Include a depth limit.
+     * @param board     the Tic Tac Toe board to play on
+     * @param ply       the maximum depth
+     */
+    public static void miniMax (Board board, int ply) {
+        MiniMax.run(board.getTurn(), board, ply);
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
